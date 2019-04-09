8c3d22ae5e96471bfee676231ae7c381fab729e6
raf924 <rafael.naciri.dev@gmail.com>
2019-04-09 12:34:13 +0200

    Radar chart first version

--- src/radar.mjs
+++ src/radar.mjs
@@ -1,5 +1,20 @@
-var w = 200;
-var h = 200;
+const w = 200;
+const h = 200;
+let datapoints1 = [20,50,60,10, 5, 20, 80, 50];
+let datapoints2 = [30, 50, 65, 20, 10, 30, 80, 50];
+
+function drawRadar(p5, datapoints) {
+    let angle = p5.TWO_PI / datapoints.length;
+    p5.beginShape();
+    let i = 0;
+    for (let a = 0; a < p5.TWO_PI; a += angle) {
+        let sx = p5.cos(a) * datapoints[i];
+        let sy = p5.sin(a) * datapoints[i];
+        p5.vertex(sx, sy);
+        i++;
+    }
+    p5.endShape(p5.CLOSE);
+}
 
 export default class {
 
@@ -11,12 +26,21 @@ export default class {
     }
 
     setup(p5) {
-        var c = p5.createCanvas(this.h, this.w);
+        const c = p5.createCanvas(this.w, this.h);
         c.position(this.x, this.y);
     };
 
     draw(p5) {
         p5.background(0, 255, 0);
-        p5.ellipse(p5.mouseX, p5.mouseY, 20, 20);
+        p5.stroke(0);
+        p5.noFill();
+        p5.translate(w / 2, h / 2);
+        for (let i = 0; i < 10; i++) {
+            drawRadar(p5, Array(datapoints2.length).fill(10 * i));
+        }
+        p5.stroke(255, 0, 0);
+        drawRadar(p5,datapoints1);
+        p5.stroke(255, 255, 0);
+        drawRadar(p5, datapoints2);
     }
 }
